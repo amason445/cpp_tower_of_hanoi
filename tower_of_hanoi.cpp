0ec00df85bf3ae6416264d6033a0081eda302f3d
amason455 <masona632@gmail.com>
2022-02-20 16:43:32 -0700

    Added Solution Floor and Cap at 0 and 62

--- tower_of_hanoi.cpp
+++ tower_of_hanoi.cpp
@@ -4,12 +4,13 @@
 #include <tuple>
 
 //contains two solutions and a test function for Knuth's Tower of Hanoi Problem
-long int RecursiveSolution(int n_discs) {
-  if (n_discs < 0) { 
-   throw "Number of Discs must be greater than or equal to zero"; 
-  }
-  else if (n_discs == 0) {
+long long int RecursiveSolution(unsigned int n_discs) {
+  if (n_discs == 0) {
    return 0;
+  }
+  //added to cap the recursion at a safe number of layers
+  else if (n_discs > 62) {
+   throw "Number of Discs Exceeds Cap of 62";
   }
   else {
    return 2 * RecursiveSolution(n_discs - 1) + 1;
@@ -17,9 +18,10 @@ long int RecursiveSolution(int n_discs) {
 }
 
 //uses std::pow()
-long int ClosedFormSolution(int n_discs) {
-  if (n_discs < 0) {
-   throw "Number of Discs must be greather than or equal to zero";
+long long int ClosedFormSolution(unsigned int n_discs) {
+  //added to cap the recursion at a safe numver of layers	
+  if (n_discs > 62) {  
+   throw "Number of Discs Exceeds Cap of 62";
   }
   else {
    return std::pow(2, n_discs) - 1;
@@ -27,22 +29,24 @@ long int ClosedFormSolution(int n_discs) {
 }
 
 //test function
-std::vector<std::tuple<long int, long int, long int>> test_function(int disc_limit) {  
-  std::vector<std::tuple<long int, long int, long int>> returnVector;
-  try {	 
+std::vector<std::tuple<int, long long int, long long int>> test_function(int disc_limit) {  
+ std::vector<std::tuple<int, long long int, long long int>> returnVector;
+ try {	 
    for(int i = 0; i <= disc_limit; i++) {
-    std::tuple<long int, long int, long int> disc_run = std::make_tuple(i, RecursiveSolution(i), ClosedFormSolution(i));
+    std::tuple<int, long long int, long long int> disc_run = std::make_tuple(i, RecursiveSolution(i), ClosedFormSolution(i));
     returnVector.push_back(disc_run);
     }   
   } 
- catch (const char* msg) { std::cerr << msg << std::endl; }
+ catch (const char* msg) { 
+	 std::cerr << msg << std::endl;
+  }
  return returnVector;
 }
 
 int main() {
 
 unsigned int input;	
-std::cout  << "Enter your maximum disc size: \n";
+std::cout  << "Enter your maximum disc amount between 0 and 62: \n";
 std::cin >> input;
 
 for(auto& tuple: test_function(input)) {std::cout << "(" << std::get<0>(tuple) << ", " << std::get<1>(tuple) << ", " << std::get<2>(tuple) << ")\n";}	
