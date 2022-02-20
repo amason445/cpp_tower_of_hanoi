78ace3e998ad95819d67764d4bd0bc690c76e956
Alex Mason <77219320+amason445@users.noreply.github.com>
2022-02-20 16:12:18 -0700

    Update tower_of_hanoi.cpp

--- tower_of_hanoi.cpp
+++ tower_of_hanoi.cpp
@@ -3,8 +3,6 @@
 #include <vector>
 #include <tuple>
 
-using namespace std;
-
 //contains two solutions and a test function for Knuth's Tower of Hanoi Problem
 long int RecursiveSolution(int n_discs) {
   if (n_discs < 0) { 
@@ -24,16 +22,16 @@ long int ClosedFormSolution(int n_discs) {
    throw "Number of Discs must be greather than or equal to zero";
   }
   else {
-   return pow(2, n_discs) - 1;
+   return std::pow(2, n_discs) - 1;
   }
 }
 
 //test function
-vector<tuple<long int, long int, long int>> test_function(int disc_limit) {  
-  vector<tuple<long int, long int, long int>> returnVector;
+std::vector<std::tuple<long int, long int, long int>> test_function(int disc_limit) {  
+  std::vector<std::tuple<long int, long int, long int>> returnVector;
   try {	 
    for(int i = 0; i <= disc_limit; i++) {
-    tuple<long int, long int, long int> disc_run = make_tuple(i, RecursiveSolution(i), ClosedFormSolution(i));
+    std::tuple<long int, long int, long int> disc_run = std::make_tuple(i, RecursiveSolution(i), ClosedFormSolution(i));
     returnVector.push_back(disc_run);
     }   
   } 
@@ -47,5 +45,5 @@ unsigned int input;
 cout  << "Enter your maximum disc size: \n";
 cin >> input;
 
-for(auto& tuple: test_function(input)) {cout << "(" << get<0>(tuple) << ", " << get<1>(tuple) << ", " << get<2>(tuple) << ")\n";}	
+for(auto& tuple: test_function(input)) {cout << "(" << std::get<0>(tuple) << ", " << std::get<1>(tuple) << ", " << std::get<2>(tuple) << ")\n";}	
 }
